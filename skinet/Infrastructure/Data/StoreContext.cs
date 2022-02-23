bb7199091780764ab3c94b25b699505f1254fbfe
Pong <nguyenphuong23314@gmail.com>
2022-02-23 23:24:10 +0700

    Add paging, sorting, searching, filtering

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -20,6 +20,20 @@ namespace Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder builder){
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach(var entityType in builder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach(var property in properties)
+                    {
+                        builder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
+
         }
     }
 }
